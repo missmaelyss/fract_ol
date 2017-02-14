031f867dda987970bf08b347fb960ccbb4e6e00b
Maelyss ARNAUD <marnaud@e1r3p9.42.fr>
2017-02-14 14:37:30 +0100

    truc en plus dans .h

--- fract_ol.h
+++ fract_ol.h
@@ -6,7 +6,7 @@
 /*   By: marnaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/06 17:42:31 by marnaud           #+#    #+#             */
-/*   Updated: 2017/02/13 14:22:01 by marnaud          ###   ########.fr       */
+/*   Updated: 2017/02/14 14:24:39 by marnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ typedef struct	s_cpl
 	double	b;
 }				t_cpl;
 
-typedef struct	s_csr
+typedef struct	s_cursor
 {
 	int		x;
 	int		y;
 }				t_csr;
 
-typedef struct	s_md
+typedef struct	s_type
 {
 	double	x_min;
 	double	x_max;
@@ -34,13 +34,27 @@ typedef struct	s_md
 	double	y_max;
 	int		nb_i;
 	int		zoom;
-}				t_md;
+}				t_type;
+
+typedef	struct	s_color
+{
+	int		b;
+	int		g;
+	int		r;
+}				t_color;
 
 typedef struct	s_env
 {
 	void	*mlx;
 	void	*win;
-	t_md	*type;
+	void	*img;
+	int		img_x;
+	int		img_y;
+	int		img_height;
+	int		img_width;
+	t_type	type;
 }				t_env;
 
-
+void			ft_initialization(t_env *ptr);
+void			ft_create_fractale(t_env *ptr);
+int				mlx_pixel_put_to_image(void	*img, t_csr cursor, t_color color);
